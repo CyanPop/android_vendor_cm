77d40ba2a40952602383265985fddb5acd85f232
RolanDroid <rolandosimoncelli8@gmail.com>
2015-11-11 19:57:34 +0100

    Updated CP Version
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -265,35 +265,12 @@ else
         CYANPOP_VERSION := $(TARGET_PRODUCT)_$(CYANPOP_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
 endif
 
+
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cyanpop.modversion=$(CYANPOP_VERSION) \
+    ro.modversion=$(CYANPOP_VERSION) \
     ro.cyanpop.version=$(VERSION)-$(CYANPOP_BUILDTYPE)
 
 -include vendor/cm-priv/keys/keys.mk
-
-CM_DISPLAY_VERSION := $(CM_VERSION)
-
-ifneq ($(PRODUCT_DEFAULT_DEV_CERTIFICATE),)
-ifneq ($(PRODUCT_DEFAULT_DEV_CERTIFICATE),build/target/product/security/testkey)
-  ifneq ($(CM_BUILDTYPE), UNOFFICIAL)
-    ifndef TARGET_VENDOR_RELEASE_BUILD_ID
-      ifneq ($(CM_EXTRAVERSION),)
-        # Remove leading dash from CM_EXTRAVERSION
-        CM_EXTRAVERSION := $(shell echo $(CM_EXTRAVERSION) | sed 's/-//')
-        TARGET_VENDOR_RELEASE_BUILD_ID := $(CM_EXTRAVERSION)
-      else
-        TARGET_VENDOR_RELEASE_BUILD_ID := $(shell date -u +%Y%m%d)
-      endif
-    else
-      TARGET_VENDOR_RELEASE_BUILD_ID := $(TARGET_VENDOR_RELEASE_BUILD_ID)
-    endif
-    CM_DISPLAY_VERSION=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(TARGET_VENDOR_RELEASE_BUILD_ID)
-  endif
-endif
-endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.cm.display.version=$(CM_DISPLAY_VERSION)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
